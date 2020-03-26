070f522cddfa54e187b900682e1da3d75f83b624
Braden Lawrence <b@bradenlawrence.com>
2020-03-26 16:38:27 -0400

    load the previously saved decks and log them

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards = append(cards, "Joker")
-	hand, remainingDeck := deal(cards, 5)
-	hand.saveToFile("my_hand")
-	remainingDeck.saveToFile("my_remainingdeck")
+	// cards := newDeck()
+	// cards = append(cards, "Joker")
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.saveToFile("my_hand")
+	// remainingDeck.saveToFile("my_remainingdeck")
+	hand := openDeck("my_hand")
+	deck := openDeck("my_remainingdeck")
+
+	hand.print()
+	deck.print()
 }
